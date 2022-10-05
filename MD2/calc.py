e0e2336f29bc7dcb2e642e5b011d6016c4975a82
AndrisBrauers <andrisbrauers@gmail.com>
2022-10-05 23:47:06 +0300

    Changed calc file

--- MD2/calc.py
+++ MD2/calc.py
@@ -16,49 +16,6 @@ def eksponenta(arg1, arg2):
     return arg1 ** arg2
 
 
-def parbaudit_ievadi(ievade):
-    
-    def check_float(arg):
-        if arg.isnumeric():
-            return float(arg)
-        else:
-            exit()
-        
-
-    if ievade == '+':
-        skaitlis_1 = input("Ievadīt pirmo vērtību summēšanai:")
-        skaitlis_1 = check_float( skaitlis_1 )
-        skaitlis_2 = input("Ievadīt otro vērtību summēšanai:")
-        skaitlis_2 = check_float( skaitlis_2 )
-        print("rezultāts: ", summa(skaitlis_1,skaitlis_2))
-    elif ievade == '-':
-        skaitlis_1 = input("Ievadīt pirmo vērtību atņemšanai:")
-        skaitlis_1 = check_float( skaitlis_1 )
-        skaitlis_2 = input("Ievadīt otro vērtību atņemšanai:")
-        skaitlis_2 = check_float( skaitlis_2 )
-        print("rezultāts: ", atnemsana(skaitlis_1,skaitlis_2))
-    elif ievade == '*':
-        skaitlis_1 = input("Ievadīt pirmo vērtību multiplicēšanai:")
-        skaitlis_1 = check_float( skaitlis_1 )
-        skaitlis_2 = input("Ievadīt otro vērtību multiplicēšanai:")
-        skaitlis_2 = check_float( skaitlis_2 )
-        print("rezultāts: ", multiplikacija(skaitlis_1,skaitlis_2))
-    elif ievade == '/':
-        skaitlis_1 = input("Ievadīt pirmo vērtību dalīšanai:")
-        skaitlis_1 = check_float( skaitlis_1 )
-        skaitlis_2 = input("Ievadīt otro vērtību dalīšanai:")
-        skaitlis_2 = check_float( skaitlis_2 )
-        print("rezultāts: ", dalisana(skaitlis_1,skaitlis_2)) 
-    elif ievade == 'eksp':
-        skaitlis_1 = input("Ievadīt bāzes vērtību eksponentai:")
-        skaitlis_1 = check_float( skaitlis_1 )
-        skaitlis_2 = input("Ievadīt pakāpes vērtību eksponentai:")
-        skaitlis_2 = check_float( skaitlis_2 )
-        print("rezultāts: ", eksponenta(skaitlis_1,skaitlis_2)) 
-    else:
-        print("operācija nav atrasta. Izvēlēties  [+ , - , * , / , eksp  ]")
-        exit()
-
 if __name__ == "__main__":
     print("Testē funkciju: ", __name__)
     assert summa(1,2) == 3
